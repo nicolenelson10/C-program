5fa197dc1a228033c0f3c31a0f41e9e74dacfba2
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2025-01-22 16:25:30 +0530

    Add files via upload

---
+++ char.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  printf("Size of integer=%ld",sizeof(int));
+  printf("\nsize of long integer=%ld",sizeof(long int));
+  printf("\nsize of short integer=%ld",sizeof(short int));
+  printf("\nsize of float=%ld",sizeof(float));
+  printf("\nsize of double=%ld",sizeof(double));
+  }
+  
+  
+
