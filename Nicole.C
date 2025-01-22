3660ac7c4b8d7fa408b3db2fc2e9c3e0bda2dcd0
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2025-01-22 16:27:50 +0530

    Add files via upload

---
+++ Nicole.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+   float number1,number2,sum;
+   printf("\nEnter two float numbers");
+   scanf("%f%f",&number1,&number2);
+   sum=number1+number2;
+   printf("%f+%f=%f/n",number1,number2,sum);
+   return 0;
+ }
